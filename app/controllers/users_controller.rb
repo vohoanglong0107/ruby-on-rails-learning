c90721fb165719544da5931b91b18c3f86f80b09
vohoanglong0107 <mamlong34@gmail.com>
2022-07-29 14:10:36 +0000

    Chapter 10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,16 @@
 class UsersController < ApplicationController
+  include Pagy::Backend
+
+  before_action :find_user, only: %i(show edit update destroy)
+  before_action :logged_in_user, except: %i(show new create)
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_user, only: :destroy
+
+  def index
+    @pagy, @users = pagy User.all
+  end
+
   def show
-    @user = User.find_by id: params[:id]
     return if @user
 
     flash[:danger] = t ".not_found"
@@ -23,9 +33,55 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = t ".success"
+      redirect_to @user
+    else
+      flash.now[:danger] = t ".failure"
+      render "edit"
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t ".success"
+    else
+      flash[:danger] = t ".failure"
+    end
+    redirect_to users_path
+  end
+
   private
+
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+
+  def find_user
+    @user = User.find_by id: params[:id]
+    return if @user.present?
+
+    flash[:danger] = t ".not_found"
+    redirect_to root_path
+  end
+
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = t ".login_required"
+    redirect_to login_url
+  end
+
+  def correct_user
+    redirect_to root_url unless current_user? @user
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
-  resources :users, only: %i(show new create)
+  resources :users
 end
