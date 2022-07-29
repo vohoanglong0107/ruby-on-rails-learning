7f8450eb758789421425c0e37d497c57de4dc4f2
vohoanglong0107 <mamlong34@gmail.com>
2022-07-29 14:10:36 +0000

    Chapter 8

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,15 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  def self.digest string
+    cost = if ActiveModel::SecurePassword.min_cost
+             BCrypt::Engine::MIN_COST
+           else
+             BCrypt::Engine.cost
+           end
+    BCrypt::Password.create(string, cost:)
+  end
+
   private
   def downcase_email
     self.email = email.downcase
